82b28e2b06afeaae325b9bc7879a6a07e34547d6
OlaChu <olenka-chuvakov@mail.ru>
2021-12-17 13:24:50 +0300

    Lesson one

--- SnakeApp/SnakeApp/Program.cs
+++ SnakeApp/SnakeApp/Program.cs
@@ -6,7 +6,7 @@ namespace SnakeApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lesson one");
             Console.ReadLine();
         }
     }
